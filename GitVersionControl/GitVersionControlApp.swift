9c8508d7430646ff59ffd98447ea521e965094f2
Leo <maxsuperleo@icloud.com>
2024-02-20 18:33:22 +0800

    Initial Commit

---
+++ GitVersionControl/GitVersionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionControlApp.swift
+//  GitVersionControl
+//
+//  Created by Leo Liu on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
